ff23aa2281e4f4e73ab91bba73b7f71b01dfdca9
Zubairyounus99 <zubairyounus99@gmail.com>
2025-02-22 23:40:03 +0500

    Delete certifications.js

--- certifications.js
+++
@@ -1,8 +0,0 @@
-export default function Certifications() {
-  return (
-    <div className="p-6 text-white bg-gray-900 min-h-screen">
-      <h1 className="text-4xl font-bold">Certifications</h1>
-      <p>Display professional certifications.</p>
-    </div>
-  );
-}
