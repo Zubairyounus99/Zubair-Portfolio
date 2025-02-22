29a65a0727bffaab1208cfbbcf1b01c06b73ba58
Zubairyounus99 <zubairyounus99@gmail.com>
2025-02-22 23:29:21 +0500

    Create about.js

---
+++ about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="p-6 text-white bg-gray-900 min-h-screen">
+      <h1 className="text-4xl font-bold">About</h1>
+      <p>Provide career summary and achievements.</p>
+    </div>
+  );
+}
