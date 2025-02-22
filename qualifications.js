d2420fa3d059c8c1ae95e621e5b3d57f9994a258
Zubairyounus99 <zubairyounus99@gmail.com>
2025-02-22 23:43:21 +0500

    Update and rename qualifications.js to contact.html

--- qualifications.js
+++
@@ -1,8 +0,0 @@
-export default function Qualification() {
-  return (
-    <div className="p-6 text-white bg-gray-900 min-h-screen">
-      <h1 className="text-4xl font-bold">Qualification</h1>
-      <p>Show educational background.</p>
-    </div>
-  );
-}
