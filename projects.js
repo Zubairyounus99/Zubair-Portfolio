10116043fc67158b56c8426bb046f3a999db4c86
Zubairyounus99 <zubairyounus99@gmail.com>
2025-02-22 23:42:23 +0500

    Update and rename projects.js to projects.html

--- projects.js
+++
@@ -1,8 +0,0 @@
-export default function Projects() {
-  return (
-    <div className="p-6 text-white bg-gray-900 min-h-screen">
-      <h1 className="text-4xl font-bold">Projects</h1>
-      <p>Showcase major engineering and AI projects.</p>
-    </div>
-  );
-}
